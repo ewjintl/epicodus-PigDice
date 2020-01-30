1800c58b2e684069484c31f43d362ae25bddc707
Elliot Jordan and Nick Gallo <example@example.com>
2020-01-30 15:39:09 -0800

    near working functionality!

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,27 @@
 // BACKEND LOGIC
 function ScoreCard() {
-    this.players = [];
-    this.currentId = 0;    
-  }
+  this.players = [];
+}
 
-function Player() {
-  this.totalScore = 0;
-  this.turnScore = 0;
-  // this.currentId = 0;
-  }
-  
+function Player(totalScore, turnScore, playerId) {
+  this.totalScore = totalScore;
+  this.turnScore = turnScore;
+  this.playerId = playerId;
+}
+
 // pushes player 
-ScoreCard.prototype.addPlayer = function(player) {
-  player.id = this.assignId();
-  this.players.push(player);
+ScoreCard.prototype.addPlayer = function (player) {
+  this.ScoreCard.push(player);
 }
 
-ScoreCard.prototype.assignId = function(player) {
-  player.currentId = ScoreCard.currentId;
-  this.currentId += 1;
-  return this.currentId;
-}
-  
-ScoreCard.prototype.findPlayer = function() {
-  for (i=0; i< this.players.length; i++) {
+// ScoreCard.prototype.assignId = function(player) {
+//   player.currentId = ScoreCard.currentId;
+//   this.currentId += 1;
+//   return this.currentId;
+// }
+
+ScoreCard.prototype.findPlayer = function () {
+  for (i = 0; i < this.players.length; i++) {
     if (this.players[i]) {
       if (this.players[i].id == id) {
         return this.players[i];
@@ -33,50 +31,61 @@ ScoreCard.prototype.findPlayer = function() {
   return false;
 }
 
-var generateDiceRoll = function() {
+var generateDiceRoll = function () {
   roll = Math.ceil((Math.random() * 6));
-  return roll;  
+  return roll;
 }
-  
 
-Player.prototype.roll = function() {
+
+Player.prototype.roll = function () {
   var currentRoll = generateDiceRoll();
   if (currentRoll !== 1) {
     this.turnScore += currentRoll;
   } else {
-   this.turnScore = 0;
+    this.turnScore = 0;
   }
   return this.turnScore;
 };
+Player.prototype.hold = function() {
+  if (this.totalScore >= 100) {
+    alert("Winner!")
+  } else {
+    this.totalScore += this.turnScore;
+  }
+  return this.totalScore;
+}
 
-ScoreCard.prototype.reset = function() {
+ScoreCard.prototype.reset = function () {
   player.totalScore = 0;
   player.turnScore = 0;
 }
-  
-// FRONTEND LOGIC
-var newGame = new ScoreCard();
 
-var playerOne = new Player();
-var playerTwo = new Player(); 
+// FRONTEND LOGIC
+var newGame = new ScoreCard(playerOne, playerTwo);
+
+var playerOne = new Player(0, 0, 0);
+var playerTwo = new Player(0, 0, 1);
+
 
 // ScoreCard.assignId(playerOne);
 console.log(playerOne);
 console.log(playerTwo);
 
-$(document).ready(function() {
-  $("button#rollBtn").on("click", function() {
+$(document).ready(function () {
+  $("button#rollBtn").on("click", function () {
     event.preventDefault();
-    $("#playerOneCurrent").text();
-  
-    $("button#holdBtn").on("click", function() {
-    event.preventDefault();
-    $("#playerOneTotal").text();
+    var rollOne = playerOne.roll();
+    $("#playerOneCurrent").text(rollOne);
+  });
 
-    $("button#resetBtn").on("click", function() {
+  $("button#holdBtn").on("click", function () {
+    event.preventDefault();
+    var holdOne = playerOne.hold();
+    $("#playerOneTotal").text(holdOne);
+  });
+
+  $("button#resetBtn").on("click", function () {
     event.preventDefault();
     reset
-
-
-});
+  });
 });
